c04af3ca347412aff8b00ae3df36f538530243f9
Saksham Rathi <22b1003@iitb.ac.in>
2024-08-27 13:43:36 +0530

    q7images

--- HW2/assignment2/7/code/myMainScript.m
+++ HW2/assignment2/7/code/myMainScript.m
@@ -1,25 +1,76 @@
-barbara = imread('../images/barbara256.png');
-kodak = imread('../images/kodak24.png');
-barbara = double(barbara);
-kodak = double(kodak);
-sigma1 = 5;
-sigma2 = 10;
+% barbara = imread('../images/barbara256.png');
+% kodak = imread('../images/kodak24.png');
+% barbara = double(barbara);
+% kodak = double(kodak);
+% sigma1 = 5;
+% sigma2 = 10;
 
-noisy_barbara_5 = barbara + sigma1 * randn(size(barbara));
-noisy_kodak_5 = kodak + sigma1 * randn(size(kodak));
+% noisy_barbara_5 = barbara + sigma1 * randn(size(barbara));
+% noisy_kodak_5 = kodak + sigma1 * randn(size(kodak));
 
-noisy_barbara_10 = barbara + sigma2 * randn(size(barbara));
-noisy_kodak_10 = kodak + sigma2 * randn(size(kodak));
+% noisy_barbara_10 = barbara + sigma2 * randn(size(barbara));
+% noisy_kodak_10 = kodak + sigma2 * randn(size(kodak));
 
-figure;
-subplot(2,3,1), imshow(uint8(barbara)), title('Original Barbara');
-subplot(2,3,2), imshow(uint8(noisy_barbara_5)), title('Barbara with Noise (\sigma = 5)');
-subplot(2,3,3), imshow(uint8(noisy_barbara_10)), title('Barbara with Noise (\sigma = 10)');
-subplot(2,3,4), imshow(uint8(kodak)), title('Original Kodak');
-subplot(2,3,5), imshow(uint8(noisy_kodak_5)), title('Kodak with Noise (\sigma = 5)');
-subplot(2,3,6), imshow(uint8(noisy_kodak_10)), title('Kodak with Noise (\sigma = 10)');
+% figure;
+% subplot(2,3,1), imshow(uint8(barbara)), title('Original Barbara');
+% subplot(2,3,2), imshow(uint8(noisy_barbara_5)), title('Barbara with Noise (\sigma = 5)');
+% subplot(2,3,3), imshow(uint8(noisy_barbara_10)), title('Barbara with Noise (\sigma = 10)');
+% subplot(2,3,4), imshow(uint8(kodak)), title('Original Kodak');
+% subplot(2,3,5), imshow(uint8(noisy_kodak_5)), title('Kodak with Noise (\sigma = 5)');
+% subplot(2,3,6), imshow(uint8(noisy_kodak_10)), title('Kodak with Noise (\sigma = 10)');
 
-imwrite(uint8(noisy_barbara_5), '../images/noisy_barbara_5.png');
-imwrite(uint8(noisy_kodak_5), '../images/noisy_kodak_5.png');
-imwrite(uint8(noisy_barbara_10), '../images/noisy_barbara_10.png');
-imwrite(uint8(noisy_kodak_10), '../images/noisy_kodak_10.png');
+% imwrite(uint8(noisy_barbara_5), '../images/noisy_barbara_5.png');
+% imwrite(uint8(noisy_kodak_5), '../images/noisy_kodak_5.png');
+% imwrite(uint8(noisy_barbara_10), '../images/noisy_barbara_10.png');
+% imwrite(uint8(noisy_kodak_10), '../images/noisy_kodak_10.png');
+
+
+% Load the noisy images (assuming you've already generated them)
+noisy_barbara_5 = imread('../images/noisy_barbara_5.png');
+noisy_kodak_5 = imread('../images/noisy_kodak_5.png');
+noisy_barbara_10 = imread('../images/noisy_barbara_10.png');
+noisy_kodak_10 = imread('../images/noisy_kodak_10.png');
+
+% Convert images to double for processing
+noisy_barbara_5 = double(noisy_barbara_5);
+noisy_kodak_5 = double(noisy_kodak_5);
+noisy_barbara_10 = double(noisy_barbara_10);
+noisy_kodak_10 = double(noisy_kodak_10);
+
+% Bilateral filter parameters
+params = [
+    2, 2;  % sigma_s = 2, sigma_r = 2
+    0.1, 0.1;  % sigma_s = 0.1, sigma_r = 0.1
+    3, 15;  % sigma_s = 3, sigma_r = 15
+];
+
+% Apply bilateral filter for each parameter set and noise level
+for p = 1:size(params, 1)
+    sigma_s = params(p, 1);
+    sigma_r = params(p, 2);
+    
+    % Apply to Barbara image with noise sigma = 5
+    filtered_barbara_5 = mybilateralfilter(noisy_barbara_5, sigma_s, sigma_r);
+    
+    % Apply to Kodak image with noise sigma = 5
+    filtered_kodak_5 = mybilateralfilter(noisy_kodak_5, sigma_s, sigma_r);
+    
+    % Apply to Barbara image with noise sigma = 10
+    filtered_barbara_10 = mybilateralfilter(noisy_barbara_10, sigma_s, sigma_r);
+    
+    % Apply to Kodak image with noise sigma = 10
+    filtered_kodak_10 = mybilateralfilter(noisy_kodak_10, sigma_s, sigma_r);
+    
+    % Display the results
+    figure;
+    subplot(2,2,1), imshow(uint8(filtered_barbara_5)), title(['Barbara (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
+    subplot(2,2,2), imshow(uint8(filtered_kodak_5)), title(['Kodak (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
+    subplot(2,2,3), imshow(uint8(filtered_barbara_10)), title(['Barbara (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
+    subplot(2,2,4), imshow(uint8(filtered_kodak_10)), title(['Kodak (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
+    
+    % Save the filtered images
+    imwrite(uint8(filtered_barbara_5), ['../images/filtered_barbara_5_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
+    imwrite(uint8(filtered_kodak_5), ['../images/filtered_kodak_5_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
+    imwrite(uint8(filtered_barbara_10), ['../images/filtered_barbara_10_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
+    imwrite(uint8(filtered_kodak_10), ['../images/filtered_kodak_10_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
+end
---
+++ HW2/assignment2/7/code/mybilateralfilter.m
@@ -0,0 +1,30 @@
+function output = mybilateralfilter(image, sigma_s, sigma_r)
+    [rows, cols] = size(image);
+    output = zeros(size(image));
+    
+    % Define the window size based on sigma_s
+    win_size = 2 * ceil(3 * sigma_s) + 1;
+    
+    % Create Gaussian spatial kernel
+    [X, Y] = meshgrid(-floor(win_size/2):floor(win_size/2), -floor(win_size/2):floor(win_size/2));
+    spatial_kernel = exp(-(X.^2 + Y.^2) / (2 * sigma_s^2));
+    
+    % Traverse the image with nested loops
+    for i = 1:rows
+        for j = 1:cols
+            % Extract local region
+            iMin = max(i-floor(win_size/2), 1);
+            iMax = min(i+floor(win_size/2), rows);
+            jMin = max(j-floor(win_size/2), 1);
+            jMax = min(j+floor(win_size/2), cols);
+            region = image(iMin:iMax, jMin:jMax);
+            
+            % Compute Gaussian range kernel
+            range_kernel = exp(-((region - image(i,j)).^2) / (2 * sigma_r^2));
+            
+            % Combined filter response
+            bilateral_filter = spatial_kernel((iMin:iMax)-i+floor(win_size/2)+1, (jMin:jMax)-j+floor(win_size/2)+1) .* range_kernel;
+            output(i,j) = sum(bilateral_filter(:) .* region(:)) / sum(bilateral_filter(:));
+        end
+    end
+end
