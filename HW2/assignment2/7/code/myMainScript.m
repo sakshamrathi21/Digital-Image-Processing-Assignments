2f31582fe6796be47038bddd4eb616355e0bdd6d
Saksham Rathi <22b1003@iitb.ac.in>
2024-08-27 13:52:14 +0530

    q7code

--- HW2/assignment2/7/code/myMainScript.m
+++ HW2/assignment2/7/code/myMainScript.m
@@ -25,50 +25,34 @@
 % imwrite(uint8(noisy_kodak_10), '../images/noisy_kodak_10.png');
 
 
-% Load the noisy images (assuming you've already generated them)
 noisy_barbara_5 = imread('../images/noisy_barbara_5.png');
 noisy_kodak_5 = imread('../images/noisy_kodak_5.png');
 noisy_barbara_10 = imread('../images/noisy_barbara_10.png');
 noisy_kodak_10 = imread('../images/noisy_kodak_10.png');
 
-% Convert images to double for processing
 noisy_barbara_5 = double(noisy_barbara_5);
 noisy_kodak_5 = double(noisy_kodak_5);
 noisy_barbara_10 = double(noisy_barbara_10);
 noisy_kodak_10 = double(noisy_kodak_10);
 
-% Bilateral filter parameters
 params = [
-    2, 2;  % sigma_s = 2, sigma_r = 2
-    0.1, 0.1;  % sigma_s = 0.1, sigma_r = 0.1
-    3, 15;  % sigma_s = 3, sigma_r = 15
+    2, 2; 
+    0.1, 0.1;
+    3, 15;
 ];
 
-% Apply bilateral filter for each parameter set and noise level
 for p = 1:size(params, 1)
     sigma_s = params(p, 1);
     sigma_r = params(p, 2);
-    
-    % Apply to Barbara image with noise sigma = 5
     filtered_barbara_5 = mybilateralfilter(noisy_barbara_5, sigma_s, sigma_r);
-    
-    % Apply to Kodak image with noise sigma = 5
     filtered_kodak_5 = mybilateralfilter(noisy_kodak_5, sigma_s, sigma_r);
-    
-    % Apply to Barbara image with noise sigma = 10
     filtered_barbara_10 = mybilateralfilter(noisy_barbara_10, sigma_s, sigma_r);
-    
-    % Apply to Kodak image with noise sigma = 10
     filtered_kodak_10 = mybilateralfilter(noisy_kodak_10, sigma_s, sigma_r);
-    
-    % Display the results
     figure;
     subplot(2,2,1), imshow(uint8(filtered_barbara_5)), title(['Barbara (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
     subplot(2,2,2), imshow(uint8(filtered_kodak_5)), title(['Kodak (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
     subplot(2,2,3), imshow(uint8(filtered_barbara_10)), title(['Barbara (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
     subplot(2,2,4), imshow(uint8(filtered_kodak_10)), title(['Kodak (\sigma_s = ', num2str(sigma_s), ', \sigma_r = ', num2str(sigma_r), ')']);
-    
-    % Save the filtered images
     imwrite(uint8(filtered_barbara_5), ['../images/filtered_barbara_5_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
     imwrite(uint8(filtered_kodak_5), ['../images/filtered_kodak_5_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
     imwrite(uint8(filtered_barbara_10), ['../images/filtered_barbara_10_sigma_s_', num2str(sigma_s), '_sigma_r_', num2str(sigma_r), '.png']);
